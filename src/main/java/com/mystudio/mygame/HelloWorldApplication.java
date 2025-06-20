058c12ca5799d54878c0627acb9743ce32240798
Patrick Twohig <patrick@namazustudios.com>
2025-06-20 09:45:49 -0700

    Updated the example to show authenticaion working with the Service layer.

--- src/main/java/com/mystudio/mygame/HelloWorldApplication.java
+++ src/main/java/com/mystudio/mygame/HelloWorldApplication.java
@@ -3,14 +3,27 @@ package com.mystudio.mygame;
 import com.mystudio.mygame.rest.HelloWorld;
 import dev.getelements.elements.sdk.annotation.ElementServiceExport;
 import dev.getelements.elements.sdk.annotation.ElementServiceImplementation;
+import dev.getelements.elements.sdk.annotation.ElementDefaultAttribute;
 import jakarta.ws.rs.core.Application;
 
 import java.util.Set;
 
-
 @ElementServiceImplementation
 @ElementServiceExport(Application.class)
 public class HelloWorldApplication extends Application {
+
+    /**
+     * This attribute enables the authentication service for this application.
+     * If set to "true", the application will use the authentication service.
+     * If set to "false", the application will not use the authentication service.
+     *
+     * Specifically when the dev.getelements.elements.auth.enabled attribute is set to "true", we will automatically
+     * install a set of filters that will ensure that the user is authenticated and applied to the service layer for
+     * all requests. This can be enabled in external configuration, or use the default annotation driven approach
+     * here.
+     */
+    @ElementDefaultAttribute("true")
+    public static final String ENABLE_AUTH = "dev.getelements.elements.auth.enabled";
 
     @Override
     public Set<Class<?>> getClasses() {
--- src/main/java/com/mystudio/mygame/rest/HelloWorld.java
+++ src/main/java/com/mystudio/mygame/rest/HelloWorld.java
@@ -1,5 +1,8 @@
 package com.mystudio.mygame.rest;
 
+import dev.getelements.elements.sdk.ElementRegistrySupplier;
+import dev.getelements.elements.sdk.model.user.User;
+import dev.getelements.elements.sdk.service.user.UserService;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
@@ -8,7 +11,25 @@ public class HelloWorld {
 
     @GET
     public String sayHello() {
-        return "Hello World!";
+
+        // Because we set the dev.getelements.elements.auth.enabled attribute to "true" in the HelloWorldApplication,
+        // the UserService will be automatically injected with the current user. This will apply an authentication
+        // filter to every request and every service that is used in this application.
+
+        final var userService = ElementRegistrySupplier.getElementLocal(HelloWorld.class)
+                .get()
+                .find("dev.getelements.elements.sdk.service")
+                .findFirst()
+                .get()
+                .getServiceLocator()
+                .getInstance(UserService.class);
+
+        final var currentUser = userService.getCurrentUser();
+
+        return User.Level.UNPRIVILEGED.equals(currentUser.getLevel())
+                ? "Sorry. You're not logged in. :("
+                : "Hello, " + currentUser.getName() + "!";
+
     }
 
 }
