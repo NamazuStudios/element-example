4f424b0c430551290f9adc88276ef21dd5e743be
ptwohig <patrick@namazustudios.com>
2025-10-20 11:27:00 -0700

    Added missing configuration.

--- src/main/java/com/mystudio/mygame/HelloWorldApplication.java
+++ src/main/java/com/mystudio/mygame/HelloWorldApplication.java
@@ -2,6 +2,7 @@ package com.mystudio.mygame;
 
 import com.mystudio.mygame.rest.HelloWorld;
 import com.mystudio.mygame.rest.HelloWithAuthentication;
+import dev.getelements.elements.sdk.annotation.ElementDefaultAttribute;
 import dev.getelements.elements.sdk.annotation.ElementServiceExport;
 import dev.getelements.elements.sdk.annotation.ElementServiceImplementation;
 import jakarta.ws.rs.core.Application;
@@ -16,6 +17,11 @@ import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 @ElementServiceExport(Application.class)
 public class HelloWorldApplication extends Application {
 
+    @ElementDefaultAttribute("true")
+    public static final String APPLICATION_NAME = "dev.getelements.elements.auth.enabled";
+
+    @ElementDefaultAttribute("example-element")
+    public static final String APPLICATION_PREFIX = "dev.getelements.elements.app.serve.prefix";
 
     /**
      * Here we register all the classes that we want to be included in the Element.
